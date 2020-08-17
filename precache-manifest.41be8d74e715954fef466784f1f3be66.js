c33ac14f82aada68e7c7a105cf956807ced845f8
tejas.savaliya <tejas14883@gmail.com>
2020-08-17 11:19:08 +0530

    Updates

---
+++ precache-manifest.41be8d74e715954fef466784f1f3be66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75850a6a2f42643bbb86c29b4a80dac8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "566c3145b3b0fde0c7f3",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "419f0fbcfb641374dbed",
+    "url": "/static/css/main.11c20889.chunk.css"
+  },
+  {
+    "revision": "566c3145b3b0fde0c7f3",
+    "url": "/static/js/2.5d3a01a2.chunk.js"
+  },
+  {
+    "revision": "af5492f10234ec8ba29a624c4fe6fb11",
+    "url": "/static/js/2.5d3a01a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "419f0fbcfb641374dbed",
+    "url": "/static/js/main.26a8e7e6.chunk.js"
+  },
+  {
+    "revision": "61237c69f4872fffe100",
+    "url": "/static/js/runtime-main.f31c91ec.js"
+  }
+]);
