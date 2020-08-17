9dd96096ce7c292b8cd16bfbfeecb82e467a6973
tejas.savaliya <tejas14883@gmail.com>
2020-08-17 23:24:05 +0530

    Updates

---
+++ precache-manifest.39cf40135d19b71d876ec628687a4971.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc253867cfa038018a23d658d3343da7",
+    "url": "/covid-19-tracker-react-app/index.html"
+  },
+  {
+    "revision": "786e2ecdd7180abb9ea2",
+    "url": "/covid-19-tracker-react-app/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "aa66b8efd1c38db547d1",
+    "url": "/covid-19-tracker-react-app/static/css/main.11c20889.chunk.css"
+  },
+  {
+    "revision": "786e2ecdd7180abb9ea2",
+    "url": "/covid-19-tracker-react-app/static/js/2.a1cbfdc7.chunk.js"
+  },
+  {
+    "revision": "af5492f10234ec8ba29a624c4fe6fb11",
+    "url": "/covid-19-tracker-react-app/static/js/2.a1cbfdc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa66b8efd1c38db547d1",
+    "url": "/covid-19-tracker-react-app/static/js/main.f8945604.chunk.js"
+  },
+  {
+    "revision": "1d71d8d0424a034b195c",
+    "url": "/covid-19-tracker-react-app/static/js/runtime-main.1ae0502c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7265e06e1c65299a25934d787c4b64cc.js"
+  "/covid-19-tracker-react-app/precache-manifest.39cf40135d19b71d876ec628687a4971.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid-19-tracker-react-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
